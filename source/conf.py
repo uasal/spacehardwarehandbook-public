c2473c49e54ae5fad19f1b1c8b26c8b76f241014
I Stefan <istefan@arizona.edu>
2025-06-30 10:51:07 +0100

    Escaping some links from link check.

--- source/conf.py
+++ source/conf.py
@@ -59,3 +59,23 @@ html_context = {
     # Set the source file suffix (for edit links)
     # 'source_suffix': _source_suffix,
 }
+
+# Parameters for the linking and link checking
+
+linkcheck_allowed_redirects = {
+# All HTTP redirections from the source URI to
+# the canonical URI will be treated as "working".
+    r'https://doi.org/10.1063/1.1149739': r'https://pubs.aip.org/rsi/article/70/5/2193/435819/Review-of-black-surfaces-for-space-borne-infrared',
+}
+
+# Ignore links that are private repos or require VPN
+linkcheck_ignore = [
+    'https://github.com/uasal/spacecoron_design_docs',
+    'https://incose.onlinelibrary.wiley.com/doi/abs/10.1002/*',
+    'https://spie.org/publications/book/*',
+    'https://www.photonics.com/Articles/Cleaning_Optics_Choosing_the_Best_Method/a32199',
+    'https://www.worldcat.org/title/machinerys-handbook/oclc/954734887',
+]
+
+# Sites where the anchoring doesn't work correctly (often a redirect issue)
+linkcheck_anchors_ignore_for_url = []
